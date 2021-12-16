8386f93a07ffb697f9bae2d3f6d0377382b52f8f
Sihula <serhiisihula@gmail.com>
2021-12-16 13:15:13 +0100

    add footer end html

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.topics__inner').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    infinite: true
+  });
+
+});
